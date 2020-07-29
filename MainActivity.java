6eaff860f453339bac17bff82d3bed400a2b0ce5
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2020-07-29 20:17:10 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.pikachu;
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.example.arapp.R;
+import com.example.batman.ArFragment;
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+    private Fragment arFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+        //detect plane and motion
+        ((ArFragment) arFragment).setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                ModelRenderable.builder()
+                        .setSource(MainActivity.this, Uri.parse("pikachu.sfb"))
+                        .build()
+                        .thenAccept(modelRenderable -> addModelToScene(anchor,modelRenderable))
+                        .exceptionally(throwable -> {
+                            AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage(throwable.getMessage())
+                                    .show();
+                            return null;
+                        });
+
+            }
+
+            private void addModelToScene(Anchor anchor, ModelRenderable modelRenderable) {
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                transformableNode.setParent(anchorNode);
+                transformableNode.setRenderable(modelRenderable);
+                ((ArFragment) arFragment).getArSceneView().getScene().addChild(anchorNode);
+                transformableNode.select();
+            }
+        });
+    }
+}
